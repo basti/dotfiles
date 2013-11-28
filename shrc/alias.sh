b9030ea078a93ef81bec26b98650d43ebfbbfd88
Slobodan Kovacevic <basti@orangeiceberg.com>
2013-11-28 13:01:48 +0100

    Add Vagrant/VitrualBox alias to completely shutdown VM.

--- shrc/alias.sh
+++ shrc/alias.sh
@@ -1,1 +1,3 @@
 alias vmslist="VBoxManage list runningvms"
+# halt vagrant vm and then kill VirtualBox completely (otherwise Parallels will complain that incompatibile VM is running)
+alias vagranthalt="vagrant halt;killall VBoxNetDHCP"
