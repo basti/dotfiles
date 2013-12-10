85d148f91e2098e7fafb6612d86ed7cc18403cf7
Slobodan Kovacevic <basti@orangeiceberg.com>
2013-12-10 20:10:23 +0100

    Go language configuration and Vagrant aliases.

--- shrc/alias.sh
+++ shrc/alias.sh
@@ -1,3 +1,4 @@
 alias vmslist="VBoxManage list runningvms"
 # halt vagrant vm and then kill VirtualBox completely (otherwise Parallels will complain that incompatibile VM is running)
 alias vagranthalt="vagrant halt;killall VBoxNetDHCP"
+alias vagrantup="vagrant up;vagrant ssh"
---
+++ shrc/go.sh
@@ -0,0 +1,5 @@
+# stuff needed for Go language
+GOPATH=$HOME/code/lab/go
+export GOPATH
+
+PATH=$PATH:$GOPATH/bin
