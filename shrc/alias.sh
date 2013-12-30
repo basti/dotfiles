5c26e1d127626370f6b20354b1b823e7662ad81f
Slobodan Kovacevic <basti@orangeiceberg.com>
2013-12-30 18:11:51 +0100

    Don't show output when killing Virtualbox DHCP.

--- shrc/alias.sh
+++ shrc/alias.sh
@@ -1,4 +1,4 @@
 alias vmslist="VBoxManage list runningvms"
 # halt vagrant vm and then kill VirtualBox completely (otherwise Parallels will complain that incompatibile VM is running)
-alias vagranthalt="vagrant halt;killall VBoxNetDHCP"
+alias vagranthalt="vagrant halt;killall VBoxNetDHCP > /dev/null"
 alias vagrantup="vagrant up;vagrant ssh"
