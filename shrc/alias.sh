ccd490e17fdac3d43768de8cafee2e74796287ab
Slobodan Kovačević <slobodan@axiomq.com>
2025-11-14 15:34:11 +0100

    Replaced Docker Desktop with Rancher Desktop

--- shrc/alias.sh
+++ shrc/alias.sh
@@ -4,5 +4,5 @@ if command -v rbenv >/dev/null 2>&1; then
 	alias rbenvignore="rbenv shell $(rbenv global)"
 fi
 
-alias irbdocker="docker run -it --rm ruby:alpine irb"
-alias irbdockerpwd="docker run -it --rm -v "$PWD":/app -w /app ruby:alpine irb"
+alias irbdocker='docker run -it --rm ruby:alpine irb'
+alias irbdockerpwd='docker run -it --rm -v "$PWD":/app -w /app ruby:alpine irb'
--- shrc/docker.sh
+++ shrc/docker.sh
@@ -1,8 +1,5 @@
 # Docker configuration
-export DOCKER_HOST=unix://$HOME/.docker/run/docker.sock
 
-# The following lines have been added by Docker Desktop to enable Docker CLI completions.
-fpath=($HOME/.docker/completions $fpath)
-autoload -Uz compinit
-compinit
-# End of Docker CLI completions
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+export PATH="/Users/basti/.rd/bin:$PATH"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
